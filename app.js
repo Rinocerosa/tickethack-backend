b5234aaede7e17a0ec73c4390f414663ce2fd087
Adrien <adrien.dupeloux@yahoo.fr>
2024-04-16 11:03:50 +0200

    Anais

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors')
-require('./models/connection');
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
